90fdfddc3cfda0537f6b6bd29f4ef601d668e84d
fersmaia10 <fersmaia10@icloud.com>
2025-09-23 22:11:58 -0300

    melhorando disposição de itens do cabeçalho e adicionando botões de compra

--- public/index.php
+++ public/index.php
@@ -1,32 +1,34 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DevStore</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<?php require_once __DIR__ . "/../app/Model/estoque.php"?>
-<body>
-    <section id="cabecalho" class="cabecalho">
-        <a href="index.php">DevStore</a>
-        <input type="search" placeholder="Busque seu produto:">
-        <a href="">Inscreva-se</a> |
-        <a href="">Entrar</a>
-        <a href="carrinho.php">Carrinho</a>
-    </section>
-    <h2>Itens disponíveis</h2>
-    <section id="produtos">
-        <div class="container">
-            <?php foreach ($db as $item): ?>
-            <div class="card">
-                <img src="<?= $item["foto"] ?>" alt="<?= $item["nomeProduto"] ?>" width="150">
-                <p><?= $item["nomeProduto"]?></p>
-                <p><?= $item["preco"]?></p>
-                <p><?= "Estoque: " . $item["quantidade"]?></p>
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>DevStore</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <?php require_once __DIR__ . "/../app/Model/estoque.php"?>
+    <body>
+        <section id="cabecalho" class="cabecalho">
+            <a href="index.php" class="devstore_link">DevStore</a>
+            <input type="search" class="busca" placeholder="Busque seu produto:">
+            <a href="" class="nova_conta">Inscreva-se</a> |
+            <a href="">Entrar</a>
+            <a href="carrinho.php">Carrinho</a>
+        </section>
+        <h2>Itens disponíveis</h2>
+        <section id="produtos">
+            <div class="container">
+                <?php foreach ($db as $item): ?>
+                <div class="card">
+                    <img src="<?= $item["foto"] ?>" alt="<?= $item["nomeProduto"] ?>" width="150">
+                    <p><?= $item["nomeProduto"]?></p>
+                    <p><?= $item["preco"]?></p>
+                    <p><?= "Estoque: " . $item["quantidade"]?></p>
+                    <input type="button" class="comprar" value="Comprar">
+                </div>
+                <?php endforeach ?>
             </div>
-            <?php endforeach ?>
-        </div>
-    </section>
-</body>
-</html>
+        </section>
+    </body>
+    <footer>DevStore</footer>
+    </html>
